494cce2a10e8176d0f6c8bb8e39e3f446699e08b
Allaa Azzam <78929333+allaa3zzaam@users.noreply.github.com>
2022-12-21 00:33:11 +0200

    Update TestRun.m

--- TestRun.m
+++ TestRun.m
@@ -1,18 +1,20 @@
-function result = IAnalyze(f, a, b)
-exactvalue = input('Please Enter the exact value');
-% compute all methods for n = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
-n = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
-y1 = zeros(1,10);
-for i=1:10
-  x = exactvalue;
-  y1(i) = Trap(f, a, b, i*10);
-  y2(i) = Simp(f, a, b, i*10);
-  y3(i) = MPoint(f, a, b, i*10);
-  end
-  plot(n, y1, 'r--',n ,y2, 'b:', n, y3, 'y*')
-  title('Graph of the methods')
-  xlabel('Number of Spaces')
-  ylabel('Approximate value')
-  legend('Trapezoidal', 'Simpson', 'Midpoint')
+% Input the lower bound
+a = str2double(input('Enter the lower bound = ', 's'));
+% Input the upper bound
+b = str2double(input('Enter the upper bound = ', 's'));
+% Input the integral function
+f = input('Enter the integral function = ');
+% Input the number of spaces
+n = str2double(input('Enter the number of spaces = ', 's'));
+% Input the name of the method to run
+method = input('Enter the name of the method to run (Trap, Simp, MPoint, or IAnalyze): ', 's');
 
+try
+    % Evaluate the chosen method using the feval function
+     arg = {f, a, b, n};
+    result = feval(method, arg);
+    % disp(result);
+catch ME
+    % If an error or exception occurs, print an error message
+    fprintf('The method you typed does not exist');
 end
