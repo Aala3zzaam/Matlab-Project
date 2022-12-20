f290f50945c899bcd09ca58733142de7fc5256cf
Allaa Azzam <78929333+allaa3zzaam@users.noreply.github.com>
2022-12-20 21:24:27 +0200

    Update IAnalyze.m

--- IAnalyze.m
+++ IAnalyze.m
@@ -1,16 +1,17 @@
-function result = IAnalyze(exactvalue, f, a, b)
-%exactvalue = input('Please Enter the exact value');
+function result = IAnalyze(f, a, b)
+exactvalue = input('Please Enter the exact value');
 % compute all methods for n = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+n = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+y1 = zeros(1,10);
 for i=1:10
-  x = exactvalue;
-  y1 = Trap(f, a, b, i*10);
-  y2 = Simp(f, a, b, i*10);
-  y3 = MPoint(f, a, b, i*10);
-  plot(x, y1, 'r--', x, y2,'b:', x, y3,'y*')
+  y1(i) = Trap(f, a, b, i*10);
+  y2(i) = Simp(f, a, b, i*10);
+  y3(i) = MPoint(f, a, b, i*10);
+  end
+  plot(n, y1, 'r--',n ,y2, 'b:', n, y3, 'y*')
   title('Graph of the methods')
-  xlabel('Exact Value')
+  xlabel('Number of Spaces')
   ylabel('Approximate value')
   legend('Trapezoidal', 'Simpson', 'Midpoint')
-end
-end
 
+end
