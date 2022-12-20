34333e5a5fda1a0929054d4ab2c9f1eec74778a8
Allaa Azzam <78929333+allaa3zzaam@users.noreply.github.com>
2022-12-20 21:24:51 +0200

    Update TestRun.m

--- TestRun.m
+++ TestRun.m
@@ -1,24 +1,20 @@
-f = input('Enter the integral function = ');
 % Input the lower bound
 a = str2double(input('Enter the lower bound = '));
 % Input the upper bound
 b = str2double(input('Enter the upper bound = '));
+% Input the integral function
+f = input('Enter the integral function = ');
 % Input the number of spaces
 n = str2double(input('Enter the number of spaces = '));
-% input the method the user wants to use
-disp('Select a function: ');
-disp('(1) Trap');
-disp('(2) Simp');
-disp('(3) MPoint');
-disp('(4) IAnalyze');
-method = input('Enter the name of the method');
-% Checking if the method the user chooses does exist
-if ~strcmpi(method, 'Trap') && ~strcmpi(method, 'Simp') && ~strcmpi(method, 'MPoint') && ~strcmpi(method, 'IAnalyze')
-    disp('Error, the method you typed doesnt exist')
-else
-% running the inputs into the chosen function through the feval function
-arg = {f,a,b,n};
-result = feval(method,arg);
-disp(result);
-% end
+% Input the name of the method to run
+method = input('Enter the name of the method to run (Trap, Simp, MPoint, or IAnalyze): ');
+
+try
+    % Evaluate the chosen method using the feval function
+    arg = {f, a, b, n};
+    result = feval(method, arg);
+    disp(result);
+catch ME
+    % If an error or exception occurs, print an error message
+    fprintf('The method you typed does not exist');
 end
